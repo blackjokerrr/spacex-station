3040d8f3c0c4002c2840d65f0961b03f2e8ab339
skaferun9 <korn3584@gmail.com>
2021-02-20 01:08:06 +0700

    commit

---
+++ src/screens/InfoScreen.js
@@ -0,0 +1,26 @@
+import React, { useState, useCallback, useEffect, createContext } from "react";
+
+
+const InfoScreen = () => {
+  
+    const [companyInfo, setCompanyInfo] = useState({});
+
+    async function fatchData() {
+        const res = await fetch(`https://api.spacexdata.com/v3/info`)
+        res
+            .json()
+            .then(res => setCompanyInfo(res))
+            
+    }
+
+    useEffect(() => {
+        fatchData();
+    }, []);
+
+
+    return (
+        <div>{companyInfo.name}</div>
+    )
+};
+
+export default InfoScreen;
